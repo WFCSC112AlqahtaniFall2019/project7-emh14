322d0e7814e9d14395a14a201938c2c6a8e17828
ethanharrison <harrem18@wfu.edu>
2019-11-07 15:58:20 -0500

    Create class Stack with .cpp and .h files

---
+++ Stack.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Ethan Harrison on 11/7/19.
+//
+
+
+#include "Data.h"
+using namespace std;
---
+++ Stack.h
@@ -0,0 +1,12 @@
+//
+// Created by Ethan Harrison on 11/7/19.
+//
+
+#ifndef PROJECT7_STACK_H
+#define PROJECT7_STACK_H
+
+class Stack {
+
+};
+
+#endif //PROJECT7_STACK_H
