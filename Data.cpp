ef1c56c3e01f4d2c084f78433c11cf00baab3720
ethanharrison <harrem18@wfu.edu>
2019-11-07 15:52:11 -0500

    Create class Data with .cpp and .h files

---
+++ Data.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Ethan Harrison on 11/7/19.
+//
+
+#include "Data.h"
+using namespace std;
+
+
---
+++ Data.h
@@ -0,0 +1,12 @@
+//
+// Created by Ethan Harrison on 11/7/19.
+//
+
+#ifndef PROJECT7_DATA_H
+#define PROJECT7_DATA_H
+
+class Data {
+
+};
+
+#endif //PROJECT7_DATA_H
