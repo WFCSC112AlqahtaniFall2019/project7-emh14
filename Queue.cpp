48719d3055e3f3f95cc67db9ffb3868ef8ca2705
ethanharrison <harrem18@wfu.edu>
2019-11-07 16:03:14 -0500

    Create class Queue with .cpp and .h files

---
+++ Queue.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Ethan Harrison on 11/7/19.
+//
+
+#include "Queue.h"
+using namespace std;
---
+++ Queue.h
@@ -0,0 +1,12 @@
+//
+// Created by Ethan Harrison on 11/7/19.
+//
+
+#ifndef PROJECT7_QUEUE_H
+#define PROJECT7_QUEUE_H
+
+class Queue {
+
+};
+
+#endif //PROJECT7_QUEUE_H
