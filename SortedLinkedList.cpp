4b80cc3146b17e493cf0159cfd14ab154ba01524
ethanharrison <harrem18@wfu.edu>
2019-11-11 15:01:04 -0500

    Create class SortedLinkedList with .cpp and .h files

---
+++ SortedLinkedList.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Ethan Harrison on 11/11/19.
+//
+
+#include "SortedLinkedList.h"
+using namespace std;
---
+++ SortedLinkedList.h
@@ -0,0 +1,14 @@
+//
+// Created by Ethan Harrison on 11/11/19.
+//
+
+#ifndef PROJECT7_SORTEDLINKEDLIST_H
+#define PROJECT7_SORTEDLINKEDLIST_H
+
+
+class SortedLinkedList {
+
+};
+
+
+#endif //PROJECT7_SORTEDLINKEDLIST_H
