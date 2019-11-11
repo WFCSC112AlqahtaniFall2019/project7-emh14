1e2b614a1449558ff7212ed935f6478dac871252
ethanharrison <harrem18@wfu.edu>
2019-11-11 16:12:07 -0500

    Complete requirements for the Data class

--- Data.cpp
+++ Data.cpp
@@ -3,6 +3,28 @@
 //
 
 #include "Data.h"
+#include <string>
+#include <iostream>
+
 using namespace std;
 
+//Overloads the output stream operator
+ostream &operator<<(ostream &os, const Data &data) {
+    os << data.title << " " << data.year << " " << data.score << " " << data.runtime << " " << data.genre << " "
+       << data.rating << " ";
+    return os;
+}
+
+//Overloads the comparison operators
+bool Data::operator>(Data scoring) {
+    if (this->score < scoring.score) {
+        return false;
+    }
+    else if (this->score > scoring.score) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
--- Data.h
+++ Data.h
@@ -4,9 +4,26 @@
 
 #ifndef PROJECT7_DATA_H
 #define PROJECT7_DATA_H
+#include <iostream>
+#include <string>
+using namespace std;
 
 class Data {
 
+public:
+    //Overloads the output stream operator
+    friend ostream& operator<<(ostream& os, const Data& data);
+
+    //Overloads the comparison operators
+    bool operator > (Data score);
+
+private:
+    string title;
+    int year;
+    int score;
+    int runtime;
+    string genre;
+    string rating;
 };
 
 #endif //PROJECT7_DATA_H
