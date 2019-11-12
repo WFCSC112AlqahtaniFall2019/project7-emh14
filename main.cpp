5e26c35cf4b656e8931f36dc268e5796d8c30ab0
ethanharrison <harrem18@wfu.edu>
2019-11-12 18:37:52 -0500

    Include every class in main

--- main.cpp
+++ main.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include "Data.h"
+#include "Stack.h"
+#include "LinkedList.h"
+#include "Queue.h"
+#include "SortedLinkedList.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
