8d4ee2c775b00ada73700baa527e2a86c89f8bd4
ethanharrison <harrem18@wfu.edu>
2019-11-12 18:41:08 -0500

    Complete requirements for the Queue class

--- Queue.cpp
+++ Queue.cpp
@@ -4,3 +4,30 @@
 
 #include "Queue.h"
 using namespace std;
+
+//Default constructor
+Queue::Queue() {
+    head = nullptr;
+    tail = nullptr;
+}
+
+//enqueue_tail member function
+void Queue::enqueue_tail(Node* input){
+    Node* newTail = new Node(input->data); //creates new Node
+    tail->next = newTail; //point to the new end of list
+    tail = newTail; //reassigns the last element
+
+}
+
+//dequeue_head member function
+bool Queue::dequeue_head(){
+    if (head){
+        Node* tempNode = head; //create temporary Node
+        head = head->next; //iterate over one position
+        delete(tempNode); //delete the temporary
+        return true;
+    }
+    else{
+        return false;
+    }
+}
--- Queue.h
+++ Queue.h
@@ -5,7 +5,23 @@
 #ifndef PROJECT7_QUEUE_H
 #define PROJECT7_QUEUE_H
 
-class Queue {
+#include "LinkedList.h"
+
+class Queue: public LinkedList {
+
+private:
+    Node* tail; //Node at end of list
+
+public:
+    //Default constructor
+    Queue();
+
+    //Inserts Node into the Queue at the tail of the list
+   void enqueue_tail(Node* input);
+
+   //removes a Data object pointed to by the head pointer
+   bool dequeue_head();
+
 
 };
 
