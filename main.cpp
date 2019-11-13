850e9be48b099d83ec5f310bf2840cafffa999a2
ethanharrison <harrem18@wfu.edu>
2019-11-12 20:09:10 -0500

    Step 1 of the driver requirements

--- main.cpp
+++ main.cpp
@@ -1,11 +1,54 @@
 #include <iostream>
+#include <fstream>
 #include "Data.h"
 #include "Stack.h"
-#include "LinkedList.h"
 #include "Queue.h"
+#include "LinkedList.h"
 #include "SortedLinkedList.h"
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    //declare fstream type variables
+    ifstream inFile;
+    ofstream outFile;
+
+    //Link csv file
+    inFile.open("MovieData.csv");
+
+    //create new Stack
+    Stack *newStack = new Stack;
+
+    //read the file by each variable line
+    while (inFile.good()) {
+
+        string title;
+        getline(inFile, title, ',');
+
+        string year;
+        getline(inFile, year, ',');
+
+        string score;
+        getline(inFile, score, ',');
+
+        string runtime;
+        getline(inFile, runtime, ',');
+
+        string genre;
+        getline(inFile, genre, ',');
+
+        string rating;
+        getline(inFile, rating, ',');
+
+        //Creates a new object for each movie read in
+        Data newData = Data(title, stoi(year), stod(score), stoi(runtime), genre, rating);
+
+        //Push to the head of the stack
+        newStack->push_head(newData);
+    }
+
+
+
     return 0;
 }
